aa0a8125ca7e483030a905fe7bd9ac136a9a5e06
Musho810 <musho810@gmail.com>
2022-05-14 21:47:28 +0400

    Triangle and figures

---
+++ src/homework/Triangle.java
@@ -0,0 +1,82 @@
+package homework;
+
+public class Triangle {
+    static int n = 10;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j < i) {
+                    System.out.print("* ");
+                } else System.out.println();
+            }
+
+        }
+        System.out.println();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= n - i; j++) {
+                if (j < n - i) {
+                    System.out.print("* ");
+                } else System.out.println();
+            }
+
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j < n - i) {
+                    System.out.print("  ");
+                } else System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j >= i) {
+                    System.out.print("* ");
+                } else System.out.print("  ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i = 1; i <= n; ++i) {
+
+            for (int j = n; j > i; --j)
+                System.out.print(" ");
+
+            for (int j = 1; j < 2 * i; ++j)
+                System.out.print("*");
+
+            System.out.println();
+        }
+
+
+        System.out.println();
+
+
+        for (int i = 1; i < n; ++i) {
+
+            for (int j = n; j > i; --j)
+                System.out.print(" ");
+
+            for (int j = 1; j < 2 * i; ++j)
+                System.out.print("*");
+
+            System.out.println();
+        }
+        for (int i = n; i >= 1; --i) {
+
+            for (int j = n; j > i; --j)
+                System.out.print(" ");
+
+            for (int j = 1; j < i * 2; ++j)
+                System.out.print("*");
+
+            System.out.println();
+        }
+    }
+}
