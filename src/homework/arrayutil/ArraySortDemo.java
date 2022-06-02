5f8b655dfbf78644e901180de8f5599693c717a0
Musho810 <musho810@gmail.com>
2022-06-02 18:37:22 +0400

    Triangle and figures

--- src/homework/arrayutil/ArraySortDemo.java
+++ src/homework/arrayutil/ArraySortDemo.java
@@ -3,6 +3,12 @@ package homework.arrayutil;
 public class ArraySortDemo {
     public static void main(String[] args) {
         int[] array = new int[]{12, 67, 4444, -98, 3, -654, 12, 5};
+        char[] spaceArray = {' ', 'c', 'a', 't', ' ', 'b', 'i', ' ', 'b', ' ', ' '};
+        char[] chars = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e'};
+        char[] chars2 = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e',};
+        char[] chars3 = {'j', 'a', 'v', 'a', 'l', 'o', 'l', 'y'};
+        char[] bobArray = {'b', 'a', 'b', 'o', 'l', 'a'};
+        char[] text = {' ', ' ', 'b', 'a', 'r', 'e', 'v', ' ', ' '};
         ArraySortWithMethod aswm = new ArraySortWithMethod();
         System.out.println("Maximum value of array is  " + aswm.max(array));
         System.out.println("Minimum value of array is   " + aswm.min(array));
@@ -11,6 +17,13 @@ public class ArraySortDemo {
         System.out.println("Quantity of Odd numbers  " + aswm.Odd(array));
         System.out.println("Quantity of Even numbers   " + aswm.Even(array));
         System.out.println( aswm.SortArray(array));
+        System.out.println(aswm.ArrayWithoutSpace(spaceArray));
+        System.out.println(" Quantity of letters O in char array is   " + aswm.quantityOfLettersO(chars));
+        System.out.println(aswm.theMiddleCharacters(chars2));
+        System.out.println("last 2 characters of arrai is  ly expression     "+ aswm.charctersLY(chars3));
+        System.out.println("the presence of symbols 'b' 'o' 'b' in the array is   " + aswm.bob(bobArray));
+        System.out.println((aswm.textWithoutSpace(text)));
+
 
     }
 }
--- src/homework/arrayutil/ArraySortWithMethod.java
+++ src/homework/arrayutil/ArraySortWithMethod.java
@@ -91,4 +91,86 @@ public class ArraySortWithMethod {
         System.out.println();
         return "The Array was sorted";
     }
+char[] ArrayWithoutSpace (char[] spaceArray){
+    int countOfSpaseFirst = 0;
+    int countOfSpaseLast = 0;
+    char spase = ' ';
+    for (int i = 0; i < spaceArray.length; i++) {
+        if (spaceArray[i] == spase) {
+            countOfSpaseFirst++;
+        } else break;
+    }
+    for (int j = spaceArray.length - 1; j >= 0; j--) {
+        if (spaceArray[j] == spase) {
+            countOfSpaseLast++;
+        } else break;
+    }
+    char [] result = new char [(spaceArray.length-countOfSpaseFirst-countOfSpaseLast)];
+    int indexResult=0;
+    for (int k=countOfSpaseFirst;k<spaceArray.length-countOfSpaseLast;k++){
+        result[indexResult]=spaceArray[k] ;
+        indexResult++;
+    }
+    return result;
 }
+int quantityOfLettersO (char[] chars) {
+    char c = 'o';
+    int countOfO = 0;
+    for (int i = 0; i < chars.length; i++) {
+        if (chars[i] == c) {
+            countOfO++;
+        }
+    }
+    return countOfO;
+}
+char[] theMiddleCharacters (char [] chars2) {
+    char[] mid1 = new char[2];
+    char[] mid2 = new char[3];
+    System.out.print(" The middle characters of array is     ");
+    if (chars2.length % 2 == 0) {
+        mid1[0] = chars2[chars2.length / 2 - 1];
+        mid1[1] = chars2[chars2.length / 2];
+        return mid1;
+    } else {
+        mid2[0] = chars2[chars2.length / 2 - 1];
+        mid2[1] = chars2[chars2.length / 2];
+        mid2[2] = chars2[chars2.length / 2 + 1];
+        return mid2;
+    }
+}
+boolean charctersLY (char [] chars3) {
+        if (chars3[chars3.length - 2] == 'l' && chars3[chars3.length - 1] == 'y') {
+            return true;
+        } else {
+           return false;
+        }
+    }
+    boolean bob (char[] bobArray){
+        char letterB = 'b';
+        int bob = 0;
+        for (int i = 0; i < bobArray.length - 2; i++) {
+            if (bobArray[i] == letterB && bobArray[i + 2] == letterB) {
+                bob++;
+            }
+        }
+        if (bob != 0) {
+            return true;
+        } else {
+           return false;
+        }
+    }
+    String textWithoutSpace (char[] text) {
+        System.out.print ("Text without space is   _   " );
+        char space = ' ';
+        for (int i = 0; i < text.length; i++) {
+            if (text[i]!=space){
+                System.out.print(text[i]);
+            }
+        }
+        System.out.println();
+        return " The characters space is deleted " ;
+    }
+}
+
+
+
